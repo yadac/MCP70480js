8862d68fca7b186e816a4d5097c98f66cca9dfdf
yosuke.adachi <yosuke.adachi@avanade.com>
2018-05-14 15:04:21 +0900

    seal

--- js/chapter5.js
+++ js/chapter5.js
@@ -1,14 +1,22 @@
 "use strict";
 
-// javascriptにおけるクラス
-// thisを付けることで自身のpropertyとして宣言できる
+
+// 5-03 javascriptにおけるクラス
+// constructor内でthisを付けると自身のpropertyとして宣言できる
 function Member(first, last) {
     this.first = first;
     this.last = last;
     this.getName = function () {
-        return this.last + " " + this.first;
+        return this.first + " " + this.last;
     }
+    // sealでプロパティの拡張禁止できる
+    // Object.seal(this);
 }
 
 var mem1 = new Member('yosuke', 'adachi');
+mem1.getGreeting = function () {
+    return `hello ${this.getName()}`;
+}
+
 console.info(mem1.getName());
+console.info(mem1.getGreeting());
