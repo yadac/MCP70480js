c8271f93ec1d9d58a103f37f8d0d8946604035bb
yosuke.adachi <yosuke.adachi@avanade.com>
2018-05-22 22:59:15 +0900

    input type month

--- js/doWork.js
+++ js/doWork.js
@@ -1,4 +1,19 @@
 self.addEventListener('message', function (e) {
-    console.log("message is: " + e.data);
-    // self.postMessage(e.data);
+
+    // just return received message to called worker instance with self.
+    // self.postMessage(e.data); 
+
+    (function () {
+        let timer = setInterval(function () {
+            console.log("message is: " + e.data);
+            // processMessage('exit');
+        }, 500);
+    })();
+
 }, false);
+
+// just function
+var processMessage = function (e) {
+    console.log("[processMessage] message is: " + e);
+
+};
--- js/webworker.js
+++ js/webworker.js
@@ -1,12 +1,13 @@
+var worker;
 setTimeout(() => {
-    var worker = new Worker('./js/doWork.js');
+    worker = new Worker('./js/doWork.js');
     // define the process on worker's onmessage event handler
     // worker.addEventListener('message', function (e) {
     //     console.log('Worker said: ', e.data);
     // }, false);
     worker.postMessage('Hello World'); // send data to our worker.
+    // worker.processMessage('exit'); // error
 }, 3000);
-
 
 (function () {
     let counter = 0;
@@ -15,6 +16,8 @@ setTimeout(() => {
         if (counter >= 5) {
             console.info("over 5");
             clearInterval(timer);
+            // terminate background task by webworker.
+            worker.terminate();
         }
         counter++;
     }, 1000);
