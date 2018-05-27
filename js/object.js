347f34a30bb8ad73c7d2f48f897704afa350b7a9
yosuke.adachi <yosuke.adachi@avanade.com>
2018-05-27 19:48:08 +0900

    prototype

--- js/object.js
+++ js/object.js
@@ -1,6 +1,21 @@
 function Device() {
 };
 
+function Circle(radius) {
+    this.radius = radius;
+    this.Circumference = function () {
+        return 2 * Math.PI * radius;
+    }
+}
+
+Circle.prototype.getArea = function () {
+    return Math.PI * this.radius * this.radius;
+}
+
+
 var device = new Device();
 Device.prototype.modelNumber = "xxx";
-console.log(device.modelNumber);
+console.log(`device.modelNumber = ${device.modelNumber}`);
+
+var circle = new Circle(3);
+console.log(`circle.getArea() = ${circle.getArea()}`);
