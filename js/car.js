b2c8aedfe8a91fc8b2396560843eb03d806c5a70
yosuke.adachi <yosuke.adachi@avanade.com>
2018-06-02 17:37:05 +0900

    cache manifest

--- js/car.js
+++ js/car.js
@@ -1,19 +1,20 @@
 $(function () {
 
     $('#carbutton').on('click', function () {
-        console.log('hello');
         car.accelerateAfterClick();
     });
+
+    vehicle.prototype.accelerateAfterClick = function () {
+        this.speed = this.speed * 2 + 1;
+        alert(this.speed);
+    };
+
+    var car = new vehicle();
+    car.accelerateAfterClick();
+
 
 });
 
 function vehicle() {
     this.speed = 0;
 }
-
-vehicle.prototype.accelerateAfterClick = function () {
-    this.speed = this.speed * 2 + 1;
-    alert(this.speed);
-};
-
-var car = new vehicle();
